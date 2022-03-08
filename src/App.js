f0ced65bc93045babcc28f9998058a8fb9eb26e9
Kalpana Tamatam <74971340+kalpana123-1@users.noreply.github.com>
2022-03-08 23:24:24 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <h1>My Motion Education Assignment</h1>
+      <h1>Motion Education Assignment</h1>
+      <div className="navbar">
+        <button className="b1">Sign Up</button>
+        <button className="b2">Log In</button>
+      </div>
     </div>
   );
 }
