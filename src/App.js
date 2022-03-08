6bd4f507a0fd29c4ecfefdbfafa5b407c93d2f98
Kalpana Tamatam <74971340+kalpana123-1@users.noreply.github.com>
2022-03-08 20:24:29 +0530

    update of motion education assignment

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <h1>My Assignment</h1>
+      <h1>My Motion Education Assignment</h1>
     </div>
   );
 }
