4f60cafd2fc0d10ff3f490b7590a5066be879eae
Kalpana Tamatam <74971340+kalpana123-1@users.noreply.github.com>
2022-03-09 06:11:38 +0530

    Added dependencies of "styled components"

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Motion Education Assignment</h1>
-      <div className="navbar">
-        <button className="b1">Sign Up</button>
-        <button className="b2">Log In</button>
-      </div>
+      <h1>Welcome to Motiong Education App</h1>
     </div>
   );
 }
