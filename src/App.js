d736fa1907eeb13c8a1ebf3931e407273ae47de1
Kalpana Tamatam <74971340+kalpana123-1@users.noreply.github.com>
2022-03-09 06:57:58 +0530

    update on styled components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./styles.css";
+import styled from "styled-components";
+import { AccountBox } from "./components/accountBox";
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default function App() {
   return (
-    <div className="App">
+    <AppContainer>
       <h1>Welcome to Motiong Education App</h1>
-    </div>
+      <AccountBox />
+    </AppContainer>
   );
 }
---
+++ src/components/accountBox/index.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import styled from "styled-components";
+
+const BoxContainer = styled.div`
+  width: 280px;
+  min-height: 550px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 19px;
+  background-color: #fff;
+  box-shadow: 0 0 2px rgba(15, 15, 15, 0.28);
+  position: relative;
+  overflow: hidden;
+`;
+
+const BackDrop = styled.div`
+  width: 160%;
+  height: 550px;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  border-radius: 50%;
+  transform: rotate(60deg);
+  top: -290px;
+  left: -70px;
+  background: rgb(241, 196, 15);
+  background: linear-gradient(
+    58deg,
+    rgba(241, 196, 15, 1) 20%,
+    rgba(243, 172, 18, 1) 100%
+  );
+`;
+
+const TopContainer = styled.div`
+  width: 50%;
+  height: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  padding: 0 1.8em;
+  padding-bottom: 5em;
+`;
+
+export function AccountBox(props) {
+  return (
+    <BoxContainer>
+      <TopContainer>
+        <BackDrop />
+      </TopContainer>
+    </BoxContainer>
+  );
+}
